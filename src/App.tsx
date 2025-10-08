cc5dcee8013f60d8ee3b79fd03173094ca778362
jihee <esder1310@naver.com>
2025-10-08 10:22:41 +0900

    docs(App.tsx) : 전역 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { RouterProvider } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { router } from './router';
 
+import './styles/reset.css';
+import './styles/global.css';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* SEO 유틸 */}
